685f1ca8542bb01e024f2e0c2cec370439a01f85
Stephen Welch <stephenwelchva@gmail.com>
2018-09-12 12:57:14 -0400

    Implement ExampleModule

--- robot/src/main/java/us/ilite/robot/modules/ExampleModule.java
+++ robot/src/main/java/us/ilite/robot/modules/ExampleModule.java
@@ -2,10 +2,14 @@ package us.ilite.robot.modules;
 
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
+import edu.wpi.first.wpilibj.Timer;
 
 public class ExampleModule extends Module {
 
+    private static final double kDelaySeconds = 0.1;
+
     ILog mLog = Logger.createLog(ExampleModule.class);
+    Timer mTimer = new Timer();
 
     @Override
     public void powerOnInit(double pNow) {
@@ -15,16 +19,26 @@ public class ExampleModule extends Module {
     @Override
     public void modeInit(double pNow) {
         mLog.error("MODE INIT");
+
+        mTimer.reset();
+        mTimer.start();
     }
 
     @Override
     public void update(double pNow) {
 
+        if(mTimer.hasPeriodPassed(kDelaySeconds)) {
+            mLog.error("ON");
+            mTimer.reset();
+        } else {
+            mLog.error("OFF");
+        }
+
     }
 
     @Override
     public void shutdown(double pNow) {
-
+         mTimer.stop();
     }
 
     @Override
